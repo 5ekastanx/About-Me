89686e85f13d90da0ce748a382494c6015cfb4ca
5ekastanx <5ekastannasirov@gmail.com>
2025-08-13 14:50:36 +0300

    обновил фото профиля

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import React from 'react';
-import './App.css'
+import './App.css';
 
 const App = () => {
+  const socialLinks = [
+    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/bekastan-nasirov-a37258301/' },
+    { name: 'WebSite', url: 'https://my-sait-b7cc.vercel.app/' },
+    { name: 'YouTube', url: 'https://www.youtube.com/@5ekastan' },
+    { name: 'GitHub', url: 'https://github.com/5ekastanx' },
+    { name: 'Telegram', url: 'https://t.me/beka_stan' },
+    { name: 'Instagram', url: 'https://www.instagram.com/5ekastan?igsh=MTN5NXJjanpjN2F4Mw==' },
+    { name: 'Blog Posts', url: 'https://blog-a-pp-ten.vercel.app/' },
+  ];
+
   return (
-    <div class='container'>
-        <div class='content'>
-            <div class='image'><img src='https://img.freepik.com/premium-photo/cartoon-man-with-glasses-and-beard-in-a-blue-shirt-generative-ai_958165-24562.jpg'/></div>
-            <span className='logo'>BEKASTAN</span>
-            <div class='btns'><div>Fullstack Developer</div><div>Digital Craftsman</div></div>
-            <a target="_blank" href="https://www.linkedin.com/in/bekastan-nasirov-a37258301/"><button class='btn'><span>LinkedIn</span></button></a>
-            <a target="_blank" href="https://my-sait-b7cc.vercel.app/"><button class='btn'><span>WebSite</span></button></a>
-            <a target="_blank" href="https://youtube.com/@tutsu.?si=VigTpzCsIfaskJNi"><button class='btn'><span>YouTube</span></button></a>
-            <a target="_blank" href="https://github.com/nasirovx"><button class='btn'><span>GitHub</span></button></a>
-            <a target="_blank" href="https://t.me/beka_stan"><button class='btn'><span>Telegram</span></button></a>
-            <a target="_blank" href="https://www.instagram.com/5ekastan?igsh=MTN5NXJjanpjN2F4Mw=="><button class='btn'><span>instagram</span></button></a>
-            <a target="_blank" href="https://blog-a-pp-ten.vercel.app/"><button class='btn'><span>Blog Posts</span></button></a>
+    <div className='app-container'>
+      <div className='card'>
+        <div className='profile-image-container'>
+          <img src="./bekastan.png" alt="Bekastan Nasirov" className='profile-image'/>
         </div>
+        
+        <h1 className='logo'>BEKASTAN</h1>
+        
+        <div className='tags'>
+          <span className='tag'>Fullstack Developer</span>
+          <span className='tag'>Digital Craftsman</span>
+        </div>
+        
+        <div className='social-buttons'>
+          {socialLinks.map((link, index) => (
+            <a 
+              key={index} 
+              href={link.url} 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className='social-button'
+            >
+              {link.name}
+            </a>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
